e1ad959a972cdb41d1465a313b107ad900ee7f7d
tai.votan <tai.vo@topdev.vn>
2021-05-26 16:55:55 +0700

    add new test case function toSlug

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,12 +8,19 @@ describe("test function utils.js", () => {
       "(*) Cài đặt bộ lọc công ty để hạn chế nhận gợi ý các công việc liên quan đến công ty/nhà tuyển dụng từ danh sách cập nhật Thị trường IT Việt Nam"
     );
   });
+  describe("toSlug", () => {
+    it("toSlug without HTML", () => {
+      const str = `(*) Cài đặt bộ lọc công ty để hạn chế nhận gợi ý các công việc liên quan`;
+      const result = toSlug(str);
+      expect(result).toMatch("cai-dat-bo-loc-cong-ty-de-han-che-nhan-goi-y-cac-cong-viec-lien-quan");
+    });
 
-  it("toSlug", () => {
-    const str = `<p class="SuggestJobsFormComponent__TextNote-sc-19k4dga-9 bsRrxN"><span> (*) Cài đặt bộ lọc công ty để hạn chế nhận gợi ý các công việc liên quan đến công ty/nhà tuyển dụng từ danh sách cập nhật</span><a style="color: rgb(211, 65, 39);">Thị trường IT Việt Nam</a></p>`;
-    const result = stripHTML(str);
-    expect(result).toMatch(
-      "cai-dat-bo-loc-cong-ty-de-han-che-nhan-goi-y-cac-cong-viec-lien-quan-den-cong-ty-nha-tuyen-dung-tu-danh-sach-cap-nhat-thi-truong-it-viet-nam"
-    );
+    it("toSlug with HTML", () => {
+      const str = `<p class="SuggestJobsFormComponent__TextNote-sc-19k4dga-9 bsRrxN"><span> (*) Cài đặt bộ lọc công ty để hạn chế nhận gợi ý các công việc liên quan đến công ty/nhà tuyển dụng từ danh sách cập nhật</span><a style="color: rgb(211, 65, 39);">Thị trường IT Việt Nam</a></p>`;
+      const result = toSlug(str);
+      expect(result).toMatch(
+        "cai-dat-bo-loc-cong-ty-de-han-che-nhan-goi-y-cac-cong-viec-lien-quan-den-cong-ty-nha-tuyen-dung-tu-danh-sach-cap-nhat-thi-truong-it-viet-nam"
+      );
+    });
   });
 });
